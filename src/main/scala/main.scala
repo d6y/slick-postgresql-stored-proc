705ad2442e46e7a3750e479be23c6073314a58fa
Richard Dallaway <richard@dallaway.com>
2017-11-01 20:23:10 +0000

    add plain SQL example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -30,6 +30,7 @@ object Example extends App {
 
   lazy val messages = TableQuery[MessageTable]
 
+  // Create the schema and populate it with data (once)
   val initDb = 
     messages.schema.create.asTry >> (
       messages.length.result flatMap {
@@ -38,10 +39,55 @@ object Example extends App {
       }
     )
 
+  // Set up a few stored functions...
+
+  // NB: $$$$ means $$ to Postgresql because $ needs to escaped as $$ in the string interpolator:
+  val createSelectProc =
+    sqlu"""
+    CREATE OR REPLACE FUNCTION numMessages(name varchar) RETURNS int
+    AS $$$$
+    DECLARE result int;
+    BEGIN
+      SELECT COUNT(*) INTO STRICT result FROM message WHERE sender = name;
+      RETURN result;
+    END;
+    $$$$ LANGUAGE plpgsql;
+    """
+
+  // An example of inserting something with (sort of) no results.
+  // NB: this is not a true proceudre as it does return something. E.g.,
+  // select systemMessage('hi');
+  // systemmessage
+  // ---------------
+  //
+  // (1 row)
+  //
+  // WIP: to run this we treat this "result" as a String
+  // Probably best practice in this particular example would be to return
+  // the number of rows inserted. (But we'd still want a void example)
+  val createInsertProc =
+    sqlu"""
+    CREATE OR REPLACE FUNCTION systemMessage(msg text) RETURNS void
+    AS $$$$
+    BEGIN
+      INSERT INTO message ("sender", "content") VALUES ('SYSTEM', msg);
+    END;
+    $$$$ LANGUAGE plpgsql;
+    """
+
+  // Plain SQL example...
+
   val program = for {
-    _       <- initDb
-    senders <- messages.filter(m => m.sender like "%Dave%").result
-  } yield senders
+    _     <- initDb
+    _     <- createSelectProc
+    _     <- createInsertProc
+    // Example procedure call which returns a value:
+    name = "Dave"
+    count <- sql"""select numMessages($name)""".as[Int]
+    // Example call that does not return a value:
+    msg = s"Counted $count messages for $name"
+     _    <- sql"""select systemMessage($msg)""".as[String] // String? See comment above on the proc definition
+  } yield count
 
   val db = Database.forConfig("example")
   try
